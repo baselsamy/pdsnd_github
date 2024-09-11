257e763d1dfc22376d2db1779ba22520aa6eb8ff
baselsamy <30785124+baselsamy@users.noreply.github.com>
2024-09-11 20:33:23 +0300

    Cleaning redundant code lines

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,6 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    print()
     
     # Get user input for city (chicago, new york city, washington)
     
@@ -60,7 +59,6 @@ def get_filters():
             print("Please enter a valid name or make sure the spelling is correct.")
         else:
             correct_answer += 1
-    print()
     print('-'*40)
     return city, month, day
 def load_data(city, month, day):
